f6032cd14e39400534750585ddd9f67bcdcf78d0
dhkim92-dev <dhkim92.dev@gmail.com>
2025-12-29 20:56:37 +0900

    cuda kernel uint32_t remove

--- examples/7.matmul-naive/CUDA/kernel.cuh
+++ examples/7.matmul-naive/CUDA/kernel.cuh
@@ -4,16 +4,16 @@ __global__ void matmul_naive(
     const float* __restrict__ A,
     const float* __restrict__ B,
     float* __restrict__ C,
-    uint32_t M,
-    uint32_t K,
-    uint32_t N
+    uint M,
+    uint K,
+    uint N
 );
 
 __global__ void matmul_tiled(
     const float* __restrict__ A,
     const float* __restrict__ B,
     float* __restrict__ C,
-    uint32_t M,
-    uint32_t K,
-    uint32_t N
+    uint M,
+    uint K,
+    uint N
 );
